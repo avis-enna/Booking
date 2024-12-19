57b2ab5b9b030bed493f4d76fe15a9087bfdcef0
Venna Venkata Siva Reddy <65495981+avis-enna@users.noreply.github.com>
2024-12-19 15:15:55 +0530

    19_Dec

---
+++ CabBook-register-2/src/main/java/com/example/demo/CabBookRegister2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookRegister2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBookRegister2Application.class, args);
+	}
+
+}
